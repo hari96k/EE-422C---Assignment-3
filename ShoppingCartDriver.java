7cab70ce56caaa605e4d152942cf1b6400076719
Bharat Kulkarni <Bharat Kulkarni>
2016-02-19 21:34:16 -0600

    Preliminary changes to driver

---
+++ ShoppingCartDriver.java
@@ -0,0 +1,39 @@
+package Assignment3;
+
+import java.io.File;
+import java.util.*;
+
+public class ShoppingCartDriver 
+	{
+
+	  public static void main(String[] args) 
+	  {
+		// TODO Auto-generated method stub
+		
+		//Open file; file name specified in args (command line)
+		  Scanner scanner = new Scanner(System.in);
+		  System.out.println("Enter the file name ");
+		  String input = scanner.next();
+		  if (0 < args.length) {
+			    String filename = args[0];
+			    File file = new File(filename);
+			  }
+		//Parse input, take appropriate actions.
+		
+		//Stub for arraylist.
+		ArrayList<Item> shoppingCart = new ArrayList<Item>(); 
+		
+		// General code example for how to iterate an array list. You will have to modify this heavily, to suit your needs.
+		Iterator<Item> i = shoppingCart.iterator();
+		while (i.hasNext()) 
+		{
+			Item temp = i.next();
+			temp.calculatePrice(); 
+			temp.printItemAttributes();
+			//This (above) works because of polymorphism: a determination is made at runtime, 
+			//based on the inherited class type, as to which method is to be invoked. Eg: If it is an instance
+			// of Grocery, it will invoke the calculatePrice () method defined in Grocery.
+		}		
+	  }
+
+}
